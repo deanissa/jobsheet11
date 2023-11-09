073000cf99fc3df8eb717a1d1ae3f4b763345b65
deanissa <deanissa604@gmail.com>
2023-11-09 14:53:58 +0700

    Nested Loop dan Rata Rata suhu

--- NestedLoop2341760187.java
+++ NestedLoop2341760187.java
@@ -6,6 +6,7 @@ public class NestedLoop2341760187 {
         Scanner sc = new Scanner (System.in);
 
         double[][] temps = new double [5][7];
+        double rata;
 
         for (int i = 0; i < temps.length; i++) {
             System.out.println("Kota ke-" +i);
@@ -18,11 +19,15 @@ public class NestedLoop2341760187 {
         }
         for (int i = 0; i < temps.length; i++) {
             System.out.print("Kota ke-" +(i+1) + ": ");
-
-            for (double temp : temps[i]) {
-                System.out.print (temp +  "");
+            double jumlah = 0;
+            for (int  j = 0; j < temps[0].length; j++) {
+                System.out.print("Hari ke-" + (j+1) + ":");
+                jumlah += temps [i][j];
+                System.out.print (temps [i][j] +" ");
             }
-            System.out.println();
+        rata = jumlah / temps.length;
+        System.out.println ("Rata Rata Suhu Kota-" + i + ": " +rata);
+        System.out.println ();
         }
     }
 }
