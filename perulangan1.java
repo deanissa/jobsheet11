b6025c12ff8cf3a89994bb8a4dbe082f323fc098
deanissa <deanissa604@gmail.com>
2023-11-09 10:50:24 +0700

    Review Perulangan Yang Lalu

---
+++ perulangan1.java
@@ -0,0 +1,17 @@
+/**
+ * perulangan1
+ */
+import java.util.Scanner;
+public class perulangan1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new  Scanner (System.in);
+
+        System.out.print ("Masukkan Nilai N :");
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            System.out.print ("*");
+        }
+    }
+}
